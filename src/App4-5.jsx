b68b560d051138df1288793b9509e869e7a83e60
qiuquan <qiuquan@qdcares.cn>
2020-03-31 22:01:56 +0800

    4-5

---
+++ src/App4-5.jsx
@@ -0,0 +1,27 @@
+import React, { useState, useMemo } from 'react';
+
+function Counter(props) {
+  return (
+      <h1>{props.count}</h1>
+  );
+}
+
+function App(params) {
+  const [count, setCount] = useState(0);
+
+  // 不传每次执行 [] 只存一次，渲染期间完成
+  const double = useMemo(() => {
+    return count*2;
+  }, [count]);
+
+    return (
+      <div>
+         <button type="button" onClick={() => {setCount(count + 1)}}>
+          click {count} ,doubles: {double}
+        </button>
+          <Counter count={double}></Counter>
+      </div>
+    );
+}
+
+export default App;
